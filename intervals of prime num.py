6bb5d3af155a0e0e9affed1b943d4e57981bc447
SUPRiYAKATTA <43086562+SUPRiYAKATTA@users.noreply.github.com>
2019-02-18 10:55:24 +0530

    Update intervals of prime num.py

--- intervals of prime num.py
+++ intervals of prime num.py
@@ -6,5 +6,5 @@ for i in range(L+1,U):
         for j in range(2,i):
             if((i%j)==0):
                 break
-            else:
-                print(i,end="")
+        else:
+            print(i,end=" ")
