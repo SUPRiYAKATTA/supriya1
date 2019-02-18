65fb35ff53bd2ad7fb04e79ea4acf347a6696b8e
SUPRiYAKATTA <43086562+SUPRiYAKATTA@users.noreply.github.com>
2019-02-18 10:26:32 +0530

    Create prime num betweenintervals.py

---
+++ prime num betweenintervals.py
@@ -0,0 +1,10 @@
+L,U=(input().split())
+L=int(L)
+U=int(U)
+for i in range(L+1,U):
+    if(i>1):
+        for j in range(2,i):
+            if((i%j)==0):
+                break
+        else:
+            print(i,end="")
