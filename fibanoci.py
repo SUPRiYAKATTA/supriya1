588748656f8feec11a118040ea6e29111eac1ddb
SUPRiYAKATTA <43086562+SUPRiYAKATTA@users.noreply.github.com>
2019-02-22 11:51:19 +0530

    Update fibanoci.py

--- fibanoci.py
+++ fibanoci.py
@@ -1,8 +1,7 @@
-f=int(input())
+s=int(input())
 a=[]
 a.append(1)
-a.append(1)
-for x in range(2,f):
+a.append(1)for x in range(2,s):
     a.append(a[x-1]+a[x-2])
 for x in range(0,len(a)):
     print(a[x]," ")
